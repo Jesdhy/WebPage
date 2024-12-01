f5eed4181a388ca4313a2a55784ed46bfd7dd5e5
Jesdhy <pinchajessy529@gmail.com>
2024-12-01 10:48:34 -0500

    feat: :fire: remove styles file
    delete styles file

--- index.js
+++ index.js
@@ -2,31 +2,11 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 3000;
-
-app.use(express.static('public'));
-
-
+const PORT = 5000;
 
 app.get('/', (req, res) => {
-    res.send(`
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Web page in EC2</title>
-        <link rel="stylesheet" href="/styles.css">
-      </head>
-      <body>
-        <h1>WEB PAGE</h1>
-        <h2>whit</h2>
-        <p>EC2</p>
-      </body>
-      </html>
-    `);
+    res.send('<h1>WEB PAGE<h2>whit<p>EC2'); 
   });
-
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
